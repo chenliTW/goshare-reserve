356f6dc5e3d7676316542fa2b6dd2dc64ffb5eaf
chenlitw <ab0897867564534231@gmail.com>
2023-01-25 01:52:44 +0800

    flask commander app and basic utils

---
+++ commander.py
@@ -0,0 +1,62 @@
+from flask import Flask,render_template,request
+import redis
+import pprint
+
+import utils
+
+r = redis.StrictRedis(host='192.168.100.200', port=6379, db=1,password="6ZSiu3JeTJ")
+#r.set('foo', 'bar')
+#print(r.get('foo'))
+
+app = Flask(__name__)
+
+def get_data():
+    data={
+        'tel_num':r.get('tel_num'),
+        'code_challenger':r.get('code_challenger'),
+        'verify_code':r.get('verify_code'),
+        'access_token':"..."+r.get('access_token')[-50:].decode(),
+        'refresh_token':"..."+r.get('refresh_token')[-50:].decode(),
+        'plate_number':r.get('plate_number'),
+        'scooter_id':r.get('scooter_id'),
+        'reserve_id':r.get('reserve_id')
+    }
+    
+    return pprint.pformat(data,sort_dicts=False)
+
+@app.route('/')
+def index():
+    return render_template('index.html',data=get_data())
+    
+@app.route('/tel')
+def tel():
+    tel_num = request.args.get('tel_num')
+    r.set('tel_num',tel_num)
+    code_challenger=utils.request_verify_code(tel_num)
+    r.set('code_challenger',code_challenger)
+    return render_template('index.html',data=get_data())
+
+@app.route('/verify')
+def verify():
+    verify_code = request.args.get('verify_code')
+    r.set('verify_code',verify_code)
+    tel_num = r.get('tel_num').decode()
+    code_challenger = r.get('code_challenger').decode()
+    access_token,refresh_token=utils.submit_verify_code(tel_num,code_challenger,verify_code)
+    r.set('access_token',access_token)
+    r.set('refresh_token',refresh_token)
+    return render_template('index.html',data=get_data())
+
+@app.route('/plate')
+def plate():
+    plate_number = request.args.get('plate_number')
+    access_token = r.get('access_token').decode()
+    scooter_id = utils.get_scooter_id(plate_number)
+    r.set('plate_number',plate_number)
+    r.set('scooter_id',scooter_id)
+    reserve_id=utils.reserve(scooter_id,access_token)
+    r.set('reserve_id',reserve_id)
+    return render_template('index.html',data=get_data())
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,101 @@
+import requests
+import json
+import time
+from string import ascii_letters,digits
+import random
+from base64 import b64encode
+from hashlib import sha256
+from os import urandom
+
+def request_verify_code(phone_number):
+    
+    random.seed(urandom(8))
+
+    h=sha256()
+    code_verifier=''.join([ random.choice(ascii_letters+digits) for _ in range(43)])
+    h.update(code_verifier.encode())
+    code_challenge=b64encode(h.digest()).decode().replace("=","").replace("+","-").replace("/","_")
+
+    header={
+        "Content-Type":"application/json",
+        "User-Agent": "okhttp/3.12.1",
+    }
+
+    data={
+        "code_challenge": code_challenge,
+        "code_challenge_method": "S256",
+        "username": "+886"+phone_number[1:]
+    }
+
+    requests.post("https://auth.ridegoshareapi.com/v1/sms_code",data=json.dumps(data),headers=header)
+
+    return code_verifier
+
+def submit_verify_code(phone_number,code_verifier,sms_code):
+
+    header={
+        "User-Agent": "okhttp/3.12.1",
+        "Authorization":"Basic NTFlZmVkNjktYjQ0MC00N2Q3LThhNTMtMmEzY2ViZTY1YzkyOg=="
+    }
+
+    data={
+        "grant_type":"sms",
+        "username":"+886"+phone_number[1:],
+        "uuid":"54ed48f9-9f9e-4162-8542-2ed8f35ebb50",
+        "sms_code":sms_code,
+        "code_verifier":code_verifier
+    }
+
+    res=requests.post("https://auth.ridegoshareapi.com/oauth/token",data=data,headers=header)
+
+    data=res.json()
+
+    access_token=data["access_token"]
+    refresh_token=data['refresh_token']
+
+    return access_token,refresh_token
+
+def get_scooter_id(plate):
+    res=requests.get("https://rental.ridegoshareapi.com/v2/cities/514f2d1d-9faf-490b-b9b2-fe8ce4dce584/scooters")
+    for scooter in res.json()['upsert_lst']:
+        if scooter['plate']==plate:
+            return scooter["id"]
+    return None
+
+def reserve(scooter_id,access_token):
+
+    data={
+        "corporate_type": 0,
+        "scooter_id": scooter_id,
+        "source": 0
+    }
+
+    header={
+        "User-Agent": "okhttp/3.12.1",
+        "Authorization":f"Bearer {access_token}",
+        "Content-Type":"application/json"
+    }
+
+    res=requests.post("https://rental.ridegoshareapi.com/v2.1/rentals",data=json.dumps(data),headers=header)
+
+    data=res.json()
+
+    return data["id"]
+
+def cancel_reserve(reserve_id,access_token):
+
+    data={
+        "action": 0,
+        "payment_method": 0
+    }
+
+    header={
+        "User-Agent": "okhttp/3.12.1",
+        "Authorization":f"Bearer {access_token}",
+        "Content-Type":"application/json"
+    }
+
+    res=requests.patch(f"https://rental.ridegoshareapi.com/v2.1/rentals/{reserve_id}",data=json.dumps(data),headers=header)
+
+    assert res.json()['id']==reserve_id
+    assert res.json()['state']==4
